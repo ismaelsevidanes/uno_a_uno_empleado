ebd3a6d278662176e5f76e32f7697d37a2d814e4
ismaelsevidanes <alum.isevidanesd@iesalixar.org>
2024-04-05 10:53:35 +0200

    proyecto creado

---
+++ src/main/java/com/uno/api/UnoAUnoEmpleadoApplication.java
@@ -0,0 +1,13 @@
+package com.uno.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoAUnoEmpleadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoAUnoEmpleadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uno/api/UnoAUnoEmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uno.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoAUnoEmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
